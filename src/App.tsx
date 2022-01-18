1568a19c48976b642dd3da4c6866c65425612052
Grammeri <reacttsxgrammer@gmail.com>
2022-01-18 16:53:53 -0600

    TDL-1-map

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./App.css"
+import {Todolist} from "./Todolist";
+
+const App = () => {
+
+    let arrForTodolist1 = [
+        {id:1, title:"HTML&CSS", isDone:false},
+        {id:2, title:"JS", isDone:true},
+        {id:3, title:"React", isDone:false},
+    ]
+
+    let arrForTodolist2 = [
+        {id:1, title:"HTML&CSS222", isDone:true},
+        {id:2, title:"JS222", isDone:false},
+        {id:3, title:"React222", isDone:true},
+    ]
+
+    return (
+        <div className={"App"}>
+            <Todolist title={"What to learn"}
+                      arrForTodolist1={arrForTodolist1}
+            />
+            <Todolist name={"What to teach"}
+                      arrForTodolist1={arrForTodolist2}
+            />
+        </div>
+    )
+}
+
+export default App //App requires default export only!
+
---
+++ src/Todolist.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+export type PropsType = {
+    title?:string
+    name?:string
+    arrForTodolist1: Array<taskType>
+   }
+
+export type taskType = {
+    id:number,
+    title: string,
+    isDone: boolean,
+}
+
+export const Todolist = (props:PropsType) => {
+    return (
+        <div>
+            <div>
+                <div>
+                    <h3>{props.title}</h3>
+                    <h3>{props.name}</h3>
+                </div>
+                <input/>
+                <button>+</button>
+
+
+                    <ul>
+
+{/*                        <li><input type={"checkbox"} checked={true}/>HTML&CSS</li>
+                        <li><input type={"checkbox"} checked={false}/>JS</li>
+                        <li><input type={"checkbox"} checked={true}/>React JS</li>*/}
+
+{/*                        <li><input type={"checkbox"}
+                                   checked={props.arrForTodolist1[0].isDone}/>{props.arrForTodolist1[0].title}</li>
+                        <li><input type={"checkbox"}
+                                   checked={props.arrForTodolist1[1].isDone}/>{props.arrForTodolist1[1].title}</li>
+                        <li><input type={"checkbox"}
+                                   checked={props.arrForTodolist1[2].isDone}/>{props.arrForTodolist1[2].title}</li>*/}
+
+                        {props.arrForTodolist1.map(m => {
+                            debugger
+                            return (<li><input type={"checkbox"} checked={m.isDone}/>{m.title}
+                            </li>)
+                        })}
+                    </ul>
+
+                <div>
+                    <button>All</button>
+                    <button>Active</button>
+                    <button>Completed</button>
+                </div>
+            </div>
+        </div>
+    )
+}
